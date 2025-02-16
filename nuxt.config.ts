b935a6ad58450d8f50147232cc1b381d793cf050
Brads3290 <github@brads3290.com>
2025-02-16 11:08:56 +1000

    Solution 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   modules: ["@formkit/nuxt", "@primevue/nuxt-module"],
   primevue: {
+    components: {
+      include: "*",
+    },
     options: {
       theme: {
         preset: Aura,
